fa8c1797fb720d24006712e2a92efa248de85003
Nicolas <casteranicolas.contact@gmail.com>
2022-09-28 17:24:38 +0200

    Update index.js

--- pages/simulateur/index.js
+++ pages/simulateur/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Formulaire from '../../components/formulaire/Formulaire';
+import HeaderForm from '../../components/headerForm/HeaderForm';
+import TextContainer from '../../components/textContainer/TextContainer';
 import styles from './styles/Simulateur.module.css';
 
 const index = () => {
     return (
         <div className={styles.content}>
-            <Formulaire />
+            <HeaderForm />
+            <div className={styles.midContent}>
+                <TextContainer />
+                <Formulaire />  
+            </div>
         </div>
     );
 };
