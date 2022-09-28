628b14f7188b4e60e938ab813545de6af258ff7d
Nicolas <casteranicolas.contact@gmail.com>
2022-09-28 17:24:47 +0200

    Update firebase.config.js

--- utils/firebase.config.js
+++ utils/firebase.config.js
@@ -1,16 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
 
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.MAPRIMERENOV_API_KEY,
-  authDomain: process.env.MAPRIMERENOV_AUTH_DOMAIN,
-  projectId: process.env.MAPRIMERENOV_PROJECT_ID,
-  storageBucket: process.env.MAPRIMERENOV_STORAGE_BUCKET,
-  messagingSenderId: process.env.MAPRIMERENOV_MESSAGING_SENDER_ID,
-  appId: process.env.MAPRIMERENOV_APP_ID
+export const firebaseConfig = {
+  apiKey: "AIzaSyANjQ3xCyjveltI081x5ceT_Xv4cUylsCc",
+  authDomain: "maprimerenov-58fdf.firebaseapp.com",
+  projectId: "maprimerenov-58fdf",
+  storageBucket: "maprimerenov-58fdf.appspot.com",
+  messagingSenderId: "855231284792",
+  appId: "1:855231284792:web:e491151c0617175c01c8cd"
 };
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const dataBase = getFirestore(app);
