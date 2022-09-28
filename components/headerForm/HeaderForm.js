bd7b46c406646db44feeba9689d435e4d848f464
Nicolas <casteranicolas.contact@gmail.com>
2022-09-28 17:24:19 +0200

    Create HeaderForm.js

---
+++ components/headerForm/HeaderForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from "./styles/headerForm.module.css";
+
+const HeaderForm = () => {
+    return (
+        <div className={styles.content}>
+            <h1 className={styles.title}>Testez votre éligibilité aux aides 2022 encadré par l’état: MaPrimeRenov’!</h1>
+            <h3 className={styles.secondTitle}>Jusu’à 80% de vos travaux d’économie d’énergie pris en charge.</h3>
+        </div>
+    );
+};
+
+export default HeaderForm;
