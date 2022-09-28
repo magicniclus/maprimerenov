dd6b1ef3f0431860f0d070288aee2276ba813531
Nicolas <casteranicolas.contact@gmail.com>
2022-09-28 10:15:56 +0200

    Create index.js

---
+++ pages/simulateur/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles/Simulateur.module.css'
+
+const index = () => {
+    return (
+        <div className='content'>
+            
+        </div>
+    );
+};
+
+export default index;
