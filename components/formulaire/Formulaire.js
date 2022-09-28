d9a269b84cf788361cb4e1c262158b3960454f92
Nicolas <casteranicolas.contact@gmail.com>
2022-09-28 11:39:05 +0200

    Update Formulaire.js

--- components/formulaire/Formulaire.js
+++ components/formulaire/Formulaire.js
@@ -8,24 +8,47 @@ const Formulaire = () => {
 
     return (
         <div className={styles.content}>
-            <form onSubmit={(e)=>handleSubmit(e)}>
-                <label>
+            <h2 className={styles.title}>Tester mon éligibilité</h2>
+            <img src='/frenchFlag.png' alt='drapeau francais' className={styles.flag} />
+            <form className={styles.formulaire} onSubmit={(e)=>handleSubmit(e)}>
+                <label className={styles.label}>
                     Votre logement *
-                    <select className={styles.select}>
+                    <select required className={styles.select}>
                         <option value="maison">Maison</option>
                         <option value="appartement">Appartement</option>
                     </select>
                 </label>
-                <label>
+                <label className={styles.label}>
                     Votre situation *
-                    <select className={styles.select}>
-                        <option value="maison">Maison</option>
-                        <option value="appartement">Appartement</option>
+                    <select required className={styles.select}>
+                        <option value="proprietaire">Proptietaire</option>
+                        <option value="locataire">Locataire</option>
                     </select>
                 </label>
-                <button type="submit">Envoyer</button>
+                <label className={styles.label}>
+                    NOM, prenom *
+                    <input className={styles.input} type="text" required />
+                </label>
+                <label className={styles.label}>
+                    Telephone *
+                    <input className={styles.input} id='phone' name="phone" type="tel" size={10} required />
+                </label>
+                <label className={styles.label}>
+                    Code Postal *
+                    <input className={styles.input}  id="zip" name="zip" type="text" pattern="^(?(^00000(|-0000))|(\d{5}(|-\d{4})))$" required/>
+                </label>
+                <label className={styles.label}>
+                    Quand souhaitez vous rélaiser ce projet ?
+                    <select required className={styles.select}>
+                        <option value="tot">Le plus tôt possible</option>
+                        <option value="threeMonth">Dans les 3 mois</option>
+                        <option value="years">Dans l&lsquo;année</option>
+                        <option value="iDontNow">Je ne sais pas</option>
+                    </select>
+                </label>
+                <button className={styles.button} type="submit">Envoyer</button>
             </form>
-            <p>En soumettant votre demande, vous acceptez que les informations saisies soient exploitées dans le cadre de la demande à la réalisation d’un bilan technique et de la relation commerciale qui peut en découler.</p>
+            <p className={styles.advertissment}>En soumettant votre demande, vous acceptez que les informations saisies soient exploitées dans le cadre de la demande à la réalisation d’un bilan technique et de la relation commerciale qui peut en découler.</p>
         </div>
     );
 };
