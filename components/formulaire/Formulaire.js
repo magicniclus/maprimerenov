49304e0717487945fb27c8d309698ba6f8426cff
Nicolas <casteranicolas.contact@gmail.com>
2022-09-28 17:24:14 +0200

    Update Formulaire.js

--- components/formulaire/Formulaire.js
+++ components/formulaire/Formulaire.js
@@ -1,41 +1,87 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from "./styles/Formulaire.module.css";
+import Image from 'next/image';
+import { dataBase } from '../../utils/firebase.config';
+import { collection, addDoc, getDocs } from 'firebase/firestore';
+import Router from 'next/router';
 
 const Formulaire = () => {
+    const [type, setType] = useState ("Maison")
+    const [situation, setSituation] = useState ("proprietaire")
+    const [name, setName] = useState("")
+    const [phone, setPhone] = useState ("")
+    const [zipCode, setZipCode] = useState ("")
+    const [when, setWhen] = useState ("Le plus tôt possible")
+
+    const databaseRef = collection(dataBase, 'prospect');
+
     const handleSubmit = (e)=>{
         e.preventDefault()
+        addDoc(databaseRef, {
+            type,
+            situation,
+            name,
+            phone,
+            zipCode,
+            when
+        })
+        .then(()=>{
+            alert("data is ok")
+            setTimeout(()=>{
+                Router.push('/thankYou')
+            }, 500)
+        })
+        .catch((err)=>{
+            console.error(err)
+        })
+    }
+    
+    useEffect(()=>{
+        getData()
+    }, [])
+
+    const getData = async ()=>{
+        await getDocs(databaseRef)
+        .then((res) => {
+            console.log(res.docs.map(data =>{
+                return data.data()
+            }));
+        })
+        .catch(err=>{
+            console.error(err)
+        })
     }
 
     return (
         <div className={styles.content}>
             <h2 className={styles.title}>Tester mon éligibilité</h2>
-            <img src='/frenchFlag.png' alt='drapeau francais' className={styles.flag} />
+            <Image src='/frenchFlag.png' alt='drapeau francais' width="100px" height="5" objectFit='cover' className={styles.flag} />
             <form className={styles.formulaire} onSubmit={(e)=>handleSubmit(e)}>
                 <label className={styles.label}>
                     Votre logement *
-                    <select required className={styles.select}>
+                     <select onChange={e => setType(e.target.value)} required className={styles.select}>
                         <option value="maison">Maison</option>
                         <option value="appartement">Appartement</option>
                     </select>
                 </label>
                 <label className={styles.label}>
                     Votre situation *
-                    <select required className={styles.select}>
+                    <select required onChange={e => setSituation(e.target.value)} className={styles.select}>
                         <option value="proprietaire">Proptietaire</option>
                         <option value="locataire">Locataire</option>
                     </select>
                 </label>
                 <label className={styles.label}>
                     NOM, prenom *
-                    <input className={styles.input} type="text" required />
+                    <input className={styles.input} type="text" onChange={e => setName(e.target.value)} required />
                 </label>
                 <label className={styles.label}>
                     Telephone *
-                    <input className={styles.input} id='phone' name="phone" type="tel" size={10} required />
+                    <input className={styles.input} onChange={e => setPhone(e.target.value)} id='phone' name="phone" type="tel" size={10} required />
                 </label>
                 <label className={styles.label}>
                     Code Postal *
-                    <input className={styles.input}  id="zip" name="zip" type="text" pattern="^(?(^00000(|-0000))|(\d{5}(|-\d{4})))$" required/>
+                    <input className={styles.input} onChange={e => setZipCode(e.target.value)}  id="zip" name="zip" type="text" pattern="^(?(^00000(|-0000))|(\d{5}(|-\d{4})))$" required/>
                 </label>
                 <label className={styles.label}>
                     Quand souhaitez vous rélaiser ce projet ?
