513cee12de437bee8a9450f855b7beab96ce4efd
Nicolas <casteranicolas.contact@gmail.com>
2022-09-29 09:11:39 +0200

    Update Nav.js

--- components/nav/Nav.js
+++ components/nav/Nav.js
@@ -1,10 +1,19 @@
+import Link from 'next/link';
 import React from 'react';
 import styles from "./styles/Nav.module.css";
 
 const Nav = () => {
     return (
         <div className={styles.nav}>
-            <h1 className={styles.title}>MaPrimRenov &lsquo;</h1>
+            <div className={styles.content}>
+                <div className={styles.leftContainer}>
+                    <h3 className={styles.title}>Eco-info.org</h3>
+                </div>
+                <div className={styles.rightContainer}>
+                    <Link href="/"><a className={styles.pro}>Vous êtes un professionel ?</a></Link>
+                    <Link href={"/"}><a className={styles.espace}>Mon espace</a></Link>
+                </div>
+            </div>
         </div>
     );
 };
