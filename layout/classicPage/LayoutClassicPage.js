0ba41e420c26b8fbefc6d4b42903622363d877f0
Nicolas <casteranicolas.contact@gmail.com>
2022-09-29 12:15:40 +0200

    Create LayoutClassicPage.js

---
+++ layout/classicPage/LayoutClassicPage.js
@@ -0,0 +1,25 @@
+import styles from "./styles/layoutClassicPage.module.css";
+import React from 'react';
+import Nav from "../../components/nav/Nav"
+import Head from 'next/head';
+import Footer from "../../components/footer/Footer";
+
+const LayoutClassicPage = (props) => {
+    return (
+        <div className={styles.content}>
+            <Head>
+                <title>{props.title}</title>
+                <meta name="description" content={props.meta} />
+            </Head>
+            <header>
+                <Nav />
+            </header>
+                {props.children}
+            <footer>
+                <Footer />
+            </footer>
+        </div>
+    );
+};
+
+export default LayoutClassicPage;
