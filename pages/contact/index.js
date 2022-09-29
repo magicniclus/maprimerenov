a25006ad2a4b67630bb158f40995229421df020d
Nicolas <casteranicolas.contact@gmail.com>
2022-09-29 14:40:58 +0200

    Update index.js

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import styles from "./styles/contact.module.css";
 import LayoutClassicPage from '../../layout/classicPage/LayoutClassicPage';
+import FormContact from '../../components/formContact/FormContact';
 
 const index = () => {
     return (
         <LayoutClassicPage meta="contact MaPrimeRenov" title="Contact">
             <main className={styles.main}>
-                <h1 className={styles.title}>Contact</h1>
-                <div className={styles.formContainer}>
-
-                </div>
+                <FormContact />
             </main>
         </LayoutClassicPage>
     );
