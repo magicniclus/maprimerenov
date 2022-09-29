296e6471ed65c6fa41c59543ce011413de9dea25
Nicolas <casteranicolas.contact@gmail.com>
2022-09-29 15:02:56 +0200

    Update FormContact.js

--- components/formContact/FormContact.js
+++ components/formContact/FormContact.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "./styles/formContact.module.css";
 import { dataBase } from '../../utils/firebase.config';
 import { collection, addDoc, getDocs } from 'firebase/firestore';
+import Router from 'next/router';
 
 const FormContact = () => {
     const databaseRef = collection(dataBase, 'contact');
+    const [name, setName] = useState("")
+    const [phone, setPhone] = useState ("")
+    const [mail, setMail] = useState ("")
+    const [sujet, setSujet] = useState ("")
+    const [contenu, setContenu] = useState ("")
 
     const handleSubmit = e=>{
         e.preventDefault()
-        
+        addDoc(databaseRef, {
+            name,
+            mail,
+            phone,
+            sujet,
+            contenu
+        })
+        .then(()=>{
+            setTimeout(()=>{
+                Router.push('/merci')
+            }, 500)
+        })
+        .catch((err)=>{
+            console.error(err)
+        })
     }
     return (
         <div className={styles.content}>
@@ -17,23 +37,23 @@ const FormContact = () => {
                 <div className={styles.imgContainer}></div>
                 <label className={styles.nameContainer}>
                     NOM, Prenom
-                    <input className={styles.input} type="text" required/>
+                    <input onChange={(e)=>setName(e.target.value)} className={styles.input} type="text" required/>
                 </label>
                 <label className={styles.nameContainer}>
                     Email
-                    <input className={styles.input} type="email" required/>
+                    <input onChange={(e)=>setMail(e.target.value)} className={styles.input} type="email" required/>
                 </label>
                 <label className={styles.nameContainer}>
                     Téléphone
-                    <input className={styles.input} type="number" minLength={10} required/>
+                    <input onChange={(e)=>setPhone(e.target.value)} className={styles.input} type="number" minLength={10} required/>
                 </label>
                 <label className={styles.nameContainer}>
                     Sujet
-                    <input className={styles.input} type="text" required/>
+                    <input onChange={(e)=>setSujet(e.target.value)} className={styles.input} type="text" required/>
                 </label>
                 <label className={styles.nameContainer}>
                     Description
-                    <textarea className={styles.textArea} id="text"></textarea>
+                    <textarea onChange={(e)=>setContenu(e.target.value)} className={styles.textArea} id="text"></textarea>
                 </label>
                 <button className={styles.button} type='submit'>Envoyer</button>
             </form>
