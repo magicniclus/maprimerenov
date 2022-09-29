f95ab4a12f8fd92c9743594ff10c06ce6520fd94
Nicolas <casteranicolas.contact@gmail.com>
2022-09-29 12:15:51 +0200

    Update index.js

--- pages/merci/index.js
+++ pages/merci/index.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import styles from "./styles/ThankYou.module.css";
-import Head from 'next/head';
-import Nav from '../../components/nav/Nav';
-import Footer from '../../components/footer/Footer';
+import styles from "./styles/merci.module.css";
+import LayoutClassicPage from '../../layout/classicPage/layoutClassicPage';
 const merci = () => {
     return (
-        <div className={styles.content}>
-            <Head>
-                <title>Remerciement</title>
-                <meta name="description" content="Remerciement poour la validation du formulaire de MaPrimeRenov." />
-            </Head>
-            <header>
-                <Nav />
-            </header>
+        <LayoutClassicPage title="Remerciement" meta="Remerciement poour la validation du formulaire de MaPrimeRenov.">
             <main className={styles.main}>
                 <h1 className={styles.title}>Merci</h1>
                 <h2 className={styles.susTitle}>Nous vous contacterons dans les 24/48h</h2>
             </main>
-            <footer>
-                <Footer />
-            </footer>
-        </div>
+        </LayoutClassicPage>
     );
 };
 
