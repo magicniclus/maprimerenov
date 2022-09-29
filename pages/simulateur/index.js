42ef4f68053fc724f6f9282b2b4c6230fa1b01f2
Nicolas <casteranicolas.contact@gmail.com>
2022-09-29 09:11:44 +0200

    Update index.js

--- pages/simulateur/index.js
+++ pages/simulateur/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Formulaire from '../../components/formulaire/Formulaire';
 import HeaderForm from '../../components/headerForm/HeaderForm';
+import Nav from '../../components/nav/Nav';
 import TextContainer from '../../components/textContainer/TextContainer';
 import VideoContainer from '../../components/videoContainer/VideoContainer';
 import Vignette from '../../components/vignette/Vignette';
@@ -9,6 +10,7 @@ import styles from './styles/Simulateur.module.css';
 const index = () => {
     return (
         <div className={styles.content}>
+            <Nav />
             <HeaderForm />
             <div className={styles.midContent}>
                 <TextContainer />
