20e30c8d1b92fab5242dc80903ba108bbeb2beae
Nicolas <casteranicolas.contact@gmail.com>
2022-09-29 14:40:53 +0200

    Update HeaderForm.js

--- components/headerForm/HeaderForm.js
+++ components/headerForm/HeaderForm.js
@@ -6,6 +6,7 @@ const HeaderForm = () => {
         <div className={styles.content}>
             <h1 className={styles.title}>Testez votre éligibilité aux aides 2022 encadré par l’état: MaPrimeRenov’!</h1>
             <h3 className={styles.secondTitle}>Jusu’à 80% de vos travaux d’économie d’énergie pris en charge.</h3>
+            <div className={styles.imgContainer}></div>
         </div>
     );
 };
