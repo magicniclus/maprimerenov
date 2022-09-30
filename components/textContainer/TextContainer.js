4d83d7b23d77951908fe8269f84bd5b853f65d8f
Nicolas <casteranicolas.contact@gmail.com>
2022-09-30 14:51:05 +0200

    Update TextContainer.js

--- components/textContainer/TextContainer.js
+++ components/textContainer/TextContainer.js
@@ -5,12 +5,12 @@ const TextContainer = () => {
     return (
         <div className={styles.content}>
             <div className={styles.topContainer}>
-                <h2 className={styles.title}>MaPrimeRenov’, c’est jusqu’à 20.000€ de prise en charge par l’état !</h2>
-                <p className={styles.text}>Bénéficiez du montant maximum subventioné pour vous. Il vous suffit de tester l’éligibilité de votre projet ! Utilisez notre simulateur GRATUIT et sans conditions de revenus pour tester votre éligibilité dès maintenant.</p>
+                <h2 className={styles.title}>MaPrimeRenov’, c’est jusqu’à <span className={styles.bolder}>20.000€</span> de prise en charge par <span className={styles.bolder}>l’état</span> !</h2>
+                <p className={styles.text}>Bénéficiez du montant maximum subventioné pour vous. Il vous suffit de tester l’éligibilité de votre projet ! Utilisez notre simulateur <span className={styles.bolder}>GRATUIT</span>  et <span className={styles.bolder}>sans conditions de revenus</span>  pour tester votre éligibilité dès maintenant.</p>
             </div>
             <div className={`${styles.bottomContainer} ${styles.two}`}>
                 <h2 className={styles.title}>Qui peut en bénéficier ?</h2>
-                <p className={styles.text}>Depuis le 1er octobre 2020, tous les propriétaires peuvent en bénificier, quels que soit leurs revenus. Les propriétaires bailleurs peuvent déposer leur dossier de demande d’aide depuis le 1er juillet 2021. En 2021 plus de 644 000 demande d’aide ont été accordé par l’état.</p>
+                <p className={styles.text}>Depuis le 1er octobre 2020, tous les propriétaires peuvent en bénificier, quels que soit leurs revenus. Les propriétaires bailleurs peuvent déposer leur dossier de demande d’aide depuis le 1er juillet 2021. En 2021 plus de <span className={styles.bolder}>644 000</span>  demande d’aide ont été accordé par l’état.</p>
             </div>
         </div>
     );
