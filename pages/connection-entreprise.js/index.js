6b25e6dfc690ea07ae716c5cf164b44c4a719518
Nicolas <casteranicolas.contact@gmail.com>
2022-09-30 14:51:14 +0200

    Create index.js

---
+++ pages/connection-entreprise.js/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from "./styles/connectionEntreprise.module.css";
+import LayoutClassicPage from '../../components/classicPage/LayoutClassicPage';
+import FormEntreprise from '../../components/formEntreprise/FormEntreprise';
+
+const index = () => {
+    return (
+        <LayoutClassicPage meta="Inscription pour les entreprises afin d'obtenir des lead avec MaPrimeRenov" >
+            <main className={styles.main}>
+                <FormEntreprise />
+            </main>
+        </LayoutClassicPage>
+    );
+};
+
+export default index;
