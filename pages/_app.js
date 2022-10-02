d1da0ef86c2b64c2f8e28ac4a08df546dfbebae9
Nicolas <casteranicolas.contact@gmail.com>
2022-10-02 14:50:18 +0200

    build

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css';
+import TagManager from 'react-gtm-module';
+import { useEffect } from 'react';
 
 
 function MyApp({ Component, pageProps }) {
+    
+    useEffect(()=>{
+        const tagManagerArgs = {
+            gtmId:"GTM-PHVP55Z"
+        }
+        
+        TagManager.initialize(tagManagerArgs)
+    }, [])
+
     return (
             <Component {...pageProps} />
     )
