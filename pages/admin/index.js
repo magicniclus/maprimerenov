022fe4b40f06a64afb3180388d865fb535c3220f
Nicolas <casteranicolas.contact@gmail.com>
2022-10-06 14:28:57 +0200

    Update index.js

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import LayoutClassicPage from "../../components/classicPage/LayoutClassicPage";
 import styles from "./styles/admin.module.css";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
 import Router from 'next/router';
-import { collection, doc, getDocs, orderBy, query } from 'firebase/firestore';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { collection, getDocs, orderBy, query } from 'firebase/firestore';
 import { dataBase } from '../../utils/firebase.config';
 
 const Admin = () => {
@@ -12,12 +12,8 @@ const Admin = () => {
 
     const auth = getAuth();
 
-    const [data, setData] = useState([])
-    const [loader, setLoader] = useState(true)
-
     onAuthStateChanged(auth, (user) => {
         if (user) {
-          const uid = user.uid;
           getData()
         } else {
             Router.push('/')
@@ -28,21 +24,21 @@ const Admin = () => {
         await getDocs(q)
         .then((res) => {
             console.log(res.docs.length);
-            res.docs.map(doc=>
-                console.log(doc.data())
+            res.docs.map(doc=>{
+                    return doc.data()
+                }
             )
         })
         .catch(err=>{
             console.error(err)
         })
-
     }
 
 
     return (
         <LayoutClassicPage title="MaPrimeRenov-info || Admin" >
             <main className={styles.main}>
-                <h1 style={{color: "black"}}>admin</h1>
+                <h1 style={{color: "black"}}>Interface Admin</h1>
             </main>
         </LayoutClassicPage>
     );
