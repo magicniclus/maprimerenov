4f99c969688115837ec858f5e9f4f0db9538b453
Nicolas <casteranicolas.contact@gmail.com>
2022-10-06 16:00:03 +0200

    Update Nav.js

--- components/nav/Nav.js
+++ components/nav/Nav.js
@@ -1,8 +1,29 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from "./styles/Nav.module.css";
+import { useDispatch, useSelector } from 'react-redux';
+import { getAuth, signOut } from "firebase/auth";
+import Router from 'next/router';
+import { switchConnect } from '../../redux/action';
 
 const Nav = () => {
+    const state = useSelector(state => state)
+    const dispatch = useDispatch()
+
+    const auth = getAuth();
+
+
+    const handleConnect = (e)=>{
+        e.preventDefault()
+        signOut(auth).
+        then(() => {
+            dispatch(switchConnect(false))
+            Router.push('/')
+        }).catch((error) => {
+          console.error(error)
+        });
+    }
+
     return (
         <div className={styles.nav} id="nav">
             <div className={styles.content}>
@@ -12,6 +33,9 @@ const Nav = () => {
                 <div className={styles.rightContainer}>
                     <Link href="/entreprise"><a className={styles.pro}>Vous êtes un professionel ?</a></Link>
                     <Link href={"/connection"}><a className={styles.espace}>Mon espace</a></Link>
+                    {
+                        auth.currentUser !== null ? <button onClick={e => handleConnect(e)}>Se deconnecter</button> : null
+                    }
                 </div>
             </div>
         </div>
