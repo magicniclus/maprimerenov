ecb93c3e68e1dd2045ed62e4ef1566a6a6512cb7
Nicolas <casteranicolas.contact@gmail.com>
2022-10-06 16:00:10 +0200

    Update index.js

--- pages/simulateur/index.js
+++ pages/simulateur/index.js
@@ -6,8 +6,12 @@ import VideoContainer from '../../components/videoContainer/VideoContainer';
 import Vignette from '../../components/vignette/Vignette';
 import styles from './styles/Simulateur.module.css';
 import LayoutClassicPage from '../../components/classicPage/LayoutClassicPage';
+import { getAuth} from "firebase/auth";
 
 const index = () => {
+
+    console.log(getAuth().currentUser);
+
     return (
         <LayoutClassicPage title="Simulateur MaPrimeRenov" meta="Simulateur gratuit pour connaitre votre éligibilité aux aides de l'état MaPrimeRenov 2022.">
             <main className={styles.main}>
