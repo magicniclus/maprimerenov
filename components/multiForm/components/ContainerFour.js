c4d875ab6171f0fd3d11df301e4a5f0bdff15bd5
Nicolas <casteranicolas.contact@gmail.com>
2022-10-11 16:56:40 +0200

    Create ContainerFour.js

---
+++ components/multiForm/components/ContainerFour.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from "../styles/multiForm.module.css";
+import BigCard from './BigCard';
+import SmallCard from './SmallCard';
+
+const ContainerFour = () => {
+    return (
+        <div className={`${styles.containerFour} ${styles.container}`}>
+            <label className={styles.label}>
+                Quel est votre principale source de chauffage actuellement :*
+            </label>
+            <div className={`${styles.cardContainerFour} ${styles.cardContainer}`}>
+                <SmallCard title="Electrique" />
+                <SmallCard title="Fioul" />
+                <SmallCard title="Gaz" />
+                <SmallCard title="Bois" />
+                <SmallCard title="Pompe à chaleur" />
+                <SmallCard title="Charbon" />
+            </div>
+        </div>
+    );
+};
+
+export default ContainerFour;
