dc1100c7db635c5d04b154c09bea05547e15fbe3
Nicolas <casteranicolas.contact@gmail.com>
2022-10-11 16:56:37 +0200

    Create ContainerFive.js

---
+++ components/multiForm/components/ContainerFive.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from "../styles/multiForm.module.css";
+import BigCard from './BigCard';
+import SmallCard from './SmallCard';
+
+const ContainerFive = () => {
+    return (
+        <div className={`${styles.containerFive} ${styles.container}`}>
+            <label className={styles.label}>
+                Quel type de travaux vous souhaitez réaliser :*
+                <span className={styles.span}>Plusieurs choix possible</span>
+            </label>
+            <div className={`${styles.cardContainerFive} ${styles.cardContainer}`}>
+                <SmallCard title="Isolation" />
+                <SmallCard title="Fenetre/Porte-fenetre" />
+                <SmallCard title="VMC double flux" />
+                <SmallCard title="Pompe à chaleur climatisation" />
+                <SmallCard title="Chauffage" />
+                <SmallCard title="Solaire/chauffe eau" />
+            </div>
+        </div>
+    );
+};
+
+export default ContainerFive;
