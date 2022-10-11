0455cd0d90fcf3496f391ad6a6a0572a2b3c698f
Nicolas <casteranicolas.contact@gmail.com>
2022-10-11 14:34:47 +0200

    Update MultiForm.js

--- components/multiForm/MultiForm.js
+++ components/multiForm/MultiForm.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./styles/multiForm.module.css";
 import Image from 'next/image';
+import ContainerOne from './components/ContainerOne';
+import { Button, LinearProgress } from '@mui/material';
 
 
 const MultiForm = () => {
+    const [value, setValue] = useState(1)
+
+    const handleContainer = ()=>{
+        switch (value) {
+            case 1:
+                return <ContainerOne />
+                break;
+        
+            case 2: 
+                return 
+                break;
+
+            default:
+                return null
+                break;
+        }
+    }
+
     return (
         <div className={styles.multiFormContainer} id="form">
-            <h2 className={styles.title}>Tester mon éligibilité</h2>
-            <Image src='/frenchFlag.png' alt='drapeau francais' width="100px" height="5" objectFit='cover' className={styles.flag} />
+            <div className={styles.topContainer}>
+                <div className={styles.progression}>
+
+                </div>
+                {/* <LinearProgress variant='determinate' value={80} /> */}
+                <h2 className={styles.title}>Tester mon éligibilité</h2>
+                <Image src='/frenchFlag.png' alt='drapeau francais' width="100px" height="5" objectFit='cover' className={styles.flag} />
+            </div>
             <form className={styles.formulaire}>
+                {
+                    handleContainer()
+                }
+                <div className={styles.buttonContainer}>
+                    <Button variant="contained" color="success">
+                        Suivant
+                    </Button>
+                    <Button variant="outlined" disabled>
+                        Retour
+                    </Button>
+                </div>
             </form>
         </div>
     );
