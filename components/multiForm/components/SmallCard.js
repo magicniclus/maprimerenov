4315e181fd7464a54afd2ce3bd575ddaf0b09d47
Nicolas <casteranicolas.contact@gmail.com>
2022-10-11 14:34:44 +0200

    Create SmallCard.js

---
+++ components/multiForm/components/SmallCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from "../styles/multiForm.module.css";
+import Image from 'next/image';
+import { Skeleton } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+
+const SmallCard = (props) => {
+    const image = props.image;
+    const altImage = props.altImage;
+    const title = props.title;
+    return (
+        <div className={styles.smallCard}>
+            <div className={styles.smallCardCheck}>
+                <FontAwesomeIcon icon={faCheck} />
+            </div>
+            {
+                image !== undefined ? <Image src={`/${image}`} alt={altImage} /> : <Skeleton variant="rectangular" width={132} height={113} />
+            }
+            <div className={styles.smallCardTitle}>{title}</div>
+        </div>
+    );
+};
+
+export default SmallCard;
