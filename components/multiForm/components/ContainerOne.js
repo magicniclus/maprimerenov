719a6b34a7c8e570ab05b835c5e1af11456b4471
Nicolas <casteranicolas.contact@gmail.com>
2022-10-11 18:43:55 +0200

    Update ContainerOne.js

--- components/multiForm/components/ContainerOne.js
+++ components/multiForm/components/ContainerOne.js
@@ -1,18 +1,28 @@
-import { containerClasses } from '@mui/system';
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "../styles/multiForm.module.css";
 import BigCard from './BigCard';
 import SmallCard from './SmallCard';
 
 const ContainerOne = () => {
+    const [cardSelected, setCardSelected] = useState(0)
+
+    console.log(cardSelected);
+
+    const handleCard = (e, value)=>{
+        e.preventDefault()
+        if(cardSelected !== value && cardSelected!==0) setCardSelected(0)
+        else setCardSelected(value)
+        console.log(cardSelected);
+    }
+
     return (
         <div className={`${styles.containerOne} ${styles.container}`}>
             <label className={styles.label}>
                 Votre projet concerne :*
             </label>
             <div className={`${styles.cardContainerOne} ${styles.cardContainer}`}>
-                <BigCard title="Une Maison" />
-                <BigCard title="Un appartement" /> 
+                <BigCard title="Une Maison" onClick={(e)=>handleCard(e, 1)} />
+                <BigCard title="Un appartement" onClick={(e)=>handleCard(e, 2)} /> 
             </div>
         </div>
     );
