617fdee74b1671cbd39c92a7c284e7d007ff5f2d
Nicolas <casteranicolas.contact@gmail.com>
2022-10-13 17:04:08 +0200

    Update ContainerFour.js

--- components/multiForm/components/ContainerFour.js
+++ components/multiForm/components/ContainerFour.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from "../styles/multiForm.module.css";
 import SmallCard from './SmallCard';
 
-const ContainerFour = () => {
+const ContainerFour = (props) => {
+    const valid = props.valid
+    const value = props.value
+
+    const [lastClick, setLastClick] = useState("")
+
+    useEffect(()=>{
+        lastClick !== "" ? valid(true) : valid(false)
+        if(lastClick !== ""){
+            valid(true)
+        }
+        value(lastClick)
+    }, [lastClick])
+
+    const handleClick = (name) =>{
+        setLastClick(lastClick !== name ? name : "")
+    }
+
+    const handleValid = (name)=>{
+        if(name === lastClick) return true
+        return false
+    }
     return (
         <div className={`${styles.containerFour} ${styles.container}`}>
             <label className={styles.label}>
                 Quel est votre principale source de chauffage actuellement :*
             </label>
             <div className={`${styles.cardContainerFour} ${styles.cardContainer}`}>
-                <SmallCard title="Electrique" />
-                <SmallCard title="Fioul" />
-                <SmallCard title="Gaz" />
-                <SmallCard title="Bois" />
-                <SmallCard title="Pompe à chaleur" />
-                <SmallCard title="Charbon" />
+                <SmallCard setLastClick={(e)=>handleClick("electrique")} valid={handleValid("electrique")} title="Electrique" />
+                <SmallCard setLastClick={(e)=>handleClick("fioul")} valid={handleValid("fioul")} title="Fioul" />
+                <SmallCard setLastClick={(e)=>handleClick("gaz")} valid={handleValid("gaz")} title="Gaz" />
+                <SmallCard setLastClick={(e)=>handleClick("bois")} valid={handleValid("bois")} title="Bois" />
+                <SmallCard setLastClick={(e)=>handleClick("pompe à chaleur")} valid={handleValid("pompe à chaleur")} title="Pompe à chaleur" />
+                <SmallCard setLastClick={(e)=>handleClick("charbon")} valid={handleValid("charbon")} title="Charbon" />
             </div>
         </div>
     );
