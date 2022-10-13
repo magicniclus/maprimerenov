a66f229b8d32a764c220fe9e62506b33df6e576b
Nicolas <casteranicolas.contact@gmail.com>
2022-10-13 17:04:06 +0200

    Update ContainerFive.js

--- components/multiForm/components/ContainerFive.js
+++ components/multiForm/components/ContainerFive.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from "../styles/multiForm.module.css";
 import SmallCard from './SmallCard';
 
-const ContainerFive = () => {
+const ContainerFive = (props) => {
+    const valid = props.valid
+    const value = props.value
+
+    const [lastClick, setLastClick] = useState("")
+
+    useEffect(()=>{
+        lastClick !== "" ? valid(true) : valid(false)
+        if(lastClick !== ""){
+            valid(true)
+        }
+        value(lastClick)
+    }, [lastClick])
+
+    const handleClick = (name) =>{
+        setLastClick(lastClick !== name ? name : "")
+    }
+
+    const handleValid = (name)=>{
+        if(name === lastClick) return true
+        return false
+    }
     return (
         <div className={`${styles.containerFive} ${styles.container}`}>
             <label className={styles.label}>
@@ -10,12 +31,12 @@ const ContainerFive = () => {
                 <span className={styles.span}>Plusieurs choix possible</span>
             </label>
             <div className={`${styles.cardContainerFive} ${styles.cardContainer}`}>
-                <SmallCard title="Isolation" />
-                <SmallCard title="Fenetre/Porte-fenetre" />
-                <SmallCard title="VMC double flux" />
-                <SmallCard title="Pompe à chaleur climatisation" />
-                <SmallCard title="Chauffage" />
-                <SmallCard title="Solaire/chauffe eau" />
+                <SmallCard setLastClick={(e)=>handleClick("isolation")} valid={handleValid("isolation")} title="Isolation" />
+                <SmallCard setLastClick={(e)=>handleClick("fenetre")} valid={handleValid("fenetre")} title="Fenetre/Porte-fenetre" />
+                <SmallCard setLastClick={(e)=>handleClick("VMC double flux")} valid={handleValid("VMC double flux")} title="VMC double flux" />
+                <SmallCard setLastClick={(e)=>handleClick("pompe à chaleur, climatisation")} valid={handleValid("pompe à chaleur, climatisation")} title="Pompe à chaleur/climatisation" />
+                <SmallCard setLastClick={(e)=>handleClick("chauffage")} valid={handleValid("chauffage")} title="Chauffage" />
+                <SmallCard setLastClick={(e)=>handleClick("solaire/chauffe eau")} valid={handleValid("solaire/chauffe eau")} title="Solaire/chauffe eau" />
             </div>
         </div>
     );
