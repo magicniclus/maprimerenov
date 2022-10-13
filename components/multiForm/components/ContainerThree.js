0ae5428120a766a3e9213057d1a064377f471da7
Nicolas <casteranicolas.contact@gmail.com>
2022-10-13 17:04:20 +0200

    Update ContainerThree.js

--- components/multiForm/components/ContainerThree.js
+++ components/multiForm/components/ContainerThree.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styles from "../styles/multiForm.module.css";
 
-const ContainerThree = () => {
+const ContainerThree = (props) => {
+
+    const valid = props.valid
+    const value = props.value
+
+    useEffect(()=>{
+        value("")
+    }, [])
+
+    const handleText = (e)=>{
+        e.preventDefault()
+        value(e.target.value)
+        if(e.target.value.lenght <= 0) valid(false)
+        valid(true)
+    }
+
     return (
         <div className={`${styles.containerThree} ${styles.container}`}>
             <label className={styles.label}>
@@ -9,7 +24,7 @@ const ContainerThree = () => {
                 <span className={styles.span}>En m2</span>
             </label>
             <div className={`${styles.cardContainerThree} ${styles.cardContainer}`}>
-                <input className={styles.input} type="number" required placeholder="100" />
+                <input onChange={e=>handleText(e)} className={styles.input} type="number" required placeholder="100" />
             </div>
         </div>
     );
