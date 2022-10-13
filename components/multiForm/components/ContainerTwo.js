18a505d9861139f2e3982c009e6f4c698b20de27
Nicolas <casteranicolas.contact@gmail.com>
2022-10-13 17:04:22 +0200

    Update ContainerTwo.js

--- components/multiForm/components/ContainerTwo.js
+++ components/multiForm/components/ContainerTwo.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from "../styles/multiForm.module.css";
 import BigCard from './BigCard';
 
-const ContainerTwo = () => {
+const ContainerTwo = (props) => {
+    const valid = props.valid
+    const value = props.value
+
+    const [lastClick, setLastClick] = useState("")
+
+    useEffect(()=>{
+        lastClick !== "" ? valid(true) : valid(false)
+        if(lastClick !== ""){
+            valid(true)
+        }
+        value(lastClick)
+    }, [lastClick])
+
+    const handleClick = (name) =>{
+        setLastClick(lastClick !== name ? name : "")
+    }
+
+    const handleValid = (name)=>{
+        if(name === lastClick) return true
+        return false
+    }
+
     return (
         <div className={`${styles.containerTwo} ${styles.container}`}>
             <label className={styles.label}>
@@ -10,9 +32,9 @@ const ContainerTwo = () => {
                 <span className={styles.span}>Cela nous permet de determiner votre éligibilité aux différentes aides</span>
             </label>
             <div className={`${styles.cardContainerTwo} ${styles.cardContainer}`}>
-                <BigCard title="Moins de deux ans" />
-                <BigCard title="Entre 2 et 10 ans" /> 
-                <BigCard title="Plus de 10 ans" /> 
+                <BigCard setLastClick={(e)=>handleClick("- de 2 ans")} valid={handleValid("- de 2 ans")} title="Moins de deux ans" />
+                <BigCard setLastClick={(e)=>handleClick("entre 2 et 10 ans")} valid={handleValid("entre 2 et 10 ans")} title="Entre 2 et 10 ans" /> 
+                <BigCard setLastClick={(e)=>handleClick("+ de 10 ans")} valid={handleValid("+ de 10 ans")} title="Plus de 10 ans" /> 
             </div>
         </div>
     );
