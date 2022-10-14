822b3eb23071fea823df737a21e4e04ba6f61401
Nicolas <casteranicolas.contact@gmail.com>
2022-10-14 18:33:12 +0200

    Update ContainerFive.js

--- components/multiForm/components/ContainerFive.js
+++ components/multiForm/components/ContainerFive.js
@@ -1,29 +1,47 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from "../styles/multiForm.module.css";
 import SmallCard from './SmallCard';
 
 const ContainerFive = (props) => {
     const valid = props.valid
     const value = props.value
+    const setIsolation = props.setIsolation
+    const setFenetre = props.setFenetre
+    const setVmc = props.setVmc
+    const setPompeAChaleurClim = props.setPompeAChaleurClim
+    const setChauffage = props.setChauffage
+    const setSolaireChauffeEau = props.setSolaireChauffeEau
 
-    const [lastClick, setLastClick] = useState("")
+    useEffect(() => {
+        valid(false)
+    }, [])
 
-    useEffect(()=>{
-        lastClick !== "" ? valid(true) : valid(false)
-        if(lastClick !== ""){
-            valid(true)
+    const handleClick = (name) => {
+        switch (name) {
+            case "isolation":
+                setIsolation(!value.isolation)
+                break;
+            case "fenetre":
+                setFenetre(!value.fenetre)
+                break;
+            case "vmc":
+                setVmc(!value.vmc)
+                break;
+            case "pompeAChaleurClim":
+                setPompeAChaleurClim(!value.pompeAChaleurClim)
+                break;
+            case "chauffage":
+                setChauffage(!value.chauffage)
+                break;
+            case "solaireChauffeEau":
+                setSolaireChauffeEau(!value.solaireChauffeEau)
+                break;
+
+            default:
+                break;
         }
-        value(lastClick)
-    }, [lastClick])
-
-    const handleClick = (name) =>{
-        setLastClick(lastClick !== name ? name : "")
     }
 
-    const handleValid = (name)=>{
-        if(name === lastClick) return true
-        return false
-    }
     return (
         <div className={`${styles.containerFive} ${styles.container}`}>
             <label className={styles.label}>
@@ -31,12 +49,12 @@ const ContainerFive = (props) => {
                 <span className={styles.span}>Plusieurs choix possible</span>
             </label>
             <div className={`${styles.cardContainerFive} ${styles.cardContainer}`}>
-                <SmallCard setLastClick={(e)=>handleClick("isolation")} valid={handleValid("isolation")} title="Isolation" />
-                <SmallCard setLastClick={(e)=>handleClick("fenetre")} valid={handleValid("fenetre")} title="Fenetre/Porte-fenetre" />
-                <SmallCard setLastClick={(e)=>handleClick("VMC double flux")} valid={handleValid("VMC double flux")} title="VMC double flux" />
-                <SmallCard setLastClick={(e)=>handleClick("pompe à chaleur, climatisation")} valid={handleValid("pompe à chaleur, climatisation")} title="Pompe à chaleur/climatisation" />
-                <SmallCard setLastClick={(e)=>handleClick("chauffage")} valid={handleValid("chauffage")} title="Chauffage" />
-                <SmallCard setLastClick={(e)=>handleClick("solaire/chauffe eau")} valid={handleValid("solaire/chauffe eau")} title="Solaire/chauffe eau" />
+                <SmallCard setLastClick={() => handleClick("isolation")} valid={value.isolation} title="Isolation" />
+                <SmallCard setLastClick={() => handleClick("fenetre")} valid={value.fenetre} title="Fenetre/Porte-fenetre" />
+                <SmallCard setLastClick={() => handleClick("vmc")} valid={value.vmc} title="VMC double flux" />
+                <SmallCard setLastClick={() => handleClick("pompeAChaleurClim")} valid={value.pompeAChaleurClim} title="Pompe à chaleur/climatisation" />
+                <SmallCard setLastClick={() => handleClick("chauffage")} valid={value.chauffage} title="Chauffage" />
+                <SmallCard setLastClick={() => handleClick("solaireChauffeEau")} valid={value.solaireChauffeEau} title="Solaire/chauffe eau" />
             </div>
         </div>
     );
