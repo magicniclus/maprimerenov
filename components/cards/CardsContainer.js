ad62197544e33853847fbf9a1964161826c8f622
Nicolas <casteranicolas.contact@gmail.com>
2022-10-17 18:53:14 +0200

    Update CardsContainer.js

--- components/cards/CardsContainer.js
+++ components/cards/CardsContainer.js
@@ -7,8 +7,8 @@ const CardsContainer = () => {
     return (
         <div className={styles.container}>
             <Cards title="644 OOO" text="+644 000 dossiers accordés" accroche="Notre travail commence par là." />
-            <Cards title="70%" text="Jusqu'à 70% d'écomie sur vos travaux et sur votre facture d'énergie" accroche="Notre plus grande satisfaction." />
-            <Cards title="20 000" text="Jusqu’à 20 000€ de prise en charge" accroche="Ca c'est le travail de l'état mais on s'en occupe !" />
+            <Cards title="70%" text="Jusqu'à 70% d'économie sur vos travaux et sur votre facture d'énergie" accroche="Notre combat à tous." />
+            <Cards title="20 000" text="Jusqu’à 20 000€ de prise en charge" accroche="Subvention accordée et encadrée par l'État." />
         </div>
     );
 };
