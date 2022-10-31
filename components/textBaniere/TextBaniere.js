3fb304ccd9f58f3f11838563417225658bdc97e8
Nicolas <casteranicolas.contact@gmail.com>
2022-10-31 18:51:33 +0100

    Update TextBaniere.js

--- components/textBaniere/TextBaniere.js
+++ components/textBaniere/TextBaniere.js
@@ -11,7 +11,7 @@ const TextBaniere = () => {
     return (
         <div className={styles.container}>
             <div className={styles.textContainer}>
-                <div className={styles.p}><span className={styles.span}>MaPrimeRénov'</span> c’est <span className={styles.span}>+644 000 primes</span> accordé par <span className={styles.span}>l’état</span></div>
+                <div className={styles.p}><span className={styles.span}>MaPrimeRénov&lsquo;</span> c’est <span className={styles.span}>+644 000 primes</span> accordé par <span className={styles.span}>l’état</span></div>
                 <div className={styles.p}>L’ensemble de ces Primes n’ont qu’un seul objectif, favoriser la transition écologique en accompagnant l’ensemble des propriétaires dans l’optimisation énergétique et économique de leur logement. MaPrimeRénov’ c’est gratuit et sans condition de revenue. </div>
                 <div className={styles.p}>Il vous suffit de tester l’éligibilité de votre projet !</div>
             </div>
