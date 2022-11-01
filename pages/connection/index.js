8ba30c7f7e52ff915bf8d0a1ae88988c18d2f778
Nicolas <casteranicolas.contact@gmail.com>
2022-11-01 10:55:25 +0100

    Update index.js

--- pages/connection/index.js
+++ pages/connection/index.js
@@ -5,22 +5,22 @@ import LayoutClassicPage from "../../components/classicPage/LayoutClassicPage"
 const Index = () => {
     const [valid, setValid] = useState(true)
 
-    const handleColorInput = (e)=>{
+    const handleColorInput = (e) => {
         e.preventDefault()
         setValid(false)
     }
 
     return (
-        <LayoutClassicPage title="Connection">
+        <LayoutClassicPage title="MaPrimeRenov-info | Se connecter" meta="Connectez-vous à votre espace dédié. Vous pouvez suivre l'avancement de votre projet MaPrimeRenov, vos artisans RGE et nous contacter directement !">
             <main className={styles.main}>
                 <div className={styles.container}>
                     <form className={styles.formulaire}>
-                    <h1 className={styles.title}>Se connecter</h1>
-                        <input style={!valid ? {border:"1px solid red"}: {border:"none"}} className={styles.input} type="email" placeholder='Email'/>
-                        <input style={!valid ? {border:"1px solid red"}: {border:"none"}} className={styles.input} type="passeword" placeholder='Mot de passe'/>
-                        <p style={!valid ? {display:"flex", color:"red"}: {display:"none"}}  className={styles.errorMessage}>Mot de passe ou Email pas valide</p>
+                        <h1 className={styles.title}>Se connecter</h1>
+                        <input style={!valid ? { border: "1px solid red" } : { border: "none" }} className={styles.input} type="email" placeholder='Email' />
+                        <input style={!valid ? { border: "1px solid red" } : { border: "none" }} className={styles.input} type="passeword" placeholder='Mot de passe' />
+                        <p style={!valid ? { display: "flex", color: "red" } : { display: "none" }} className={styles.errorMessage}>Mot de passe ou Email pas valide</p>
                         <p className={styles.recuperation}>Mot de passe oublié ?</p>
-                        <button onClick={(e)=>handleColorInput(e)} className={styles.button} type='submit'>Se connecter</button>
+                        <button onClick={(e) => handleColorInput(e)} className={styles.button} type='submit'>Se connecter</button>
                     </form>
                 </div>
             </main>
