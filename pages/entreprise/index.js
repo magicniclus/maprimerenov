b7a34a50c0a5c31dd90baa0d722be7bd8934247f
Nicolas <casteranicolas.contact@gmail.com>
2022-11-01 10:55:27 +0100

    Update index.js

--- pages/entreprise/index.js
+++ pages/entreprise/index.js
@@ -5,7 +5,7 @@ import FormEntreprise from '../../components/formEntreprise/FormEntreprise';
 
 const index = () => {
     return (
-        <LayoutClassicPage title="Inscription Entreprise" meta="Inscription pour les entreprises afin d'obtenir des lead avec MaPrimeRenov" >
+        <LayoutClassicPage title="MaPrimeRenov-info | Espace Entreprise" meta="Artisan RGE, devenez partenaire et faite vous accompagner pour proposer MaPrimeRenov à vos clients! Trouvez des clients qualifiés." >
             <main className={styles.main}>
                 <FormEntreprise />
             </main>
