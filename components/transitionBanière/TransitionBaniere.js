f23ce9604fa185e344b08e7518203be2302c28c9
Nicolas <casteranicolas.contact@gmail.com>
2022-11-03 15:43:33 +0100

    Update TransitionBaniere.js

--- components/transitionBanière/TransitionBaniere.js
+++ components/transitionBanière/TransitionBaniere.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import styles from "./styles/transitionBaniere.module.css";
 
-const TransitionBaniere = () => {
+const TransitionBaniere = (props) => {
     return (
-        <div className={styles.container}>
-            <h3 className={styles.title}>
-                Avec <span className={styles.span}>MaPrimeRénov’</span>, vous favorisez
-                la <span className={styles.span}>transition écologique</span>.
-            </h3>
-        </div>
+        <div
+            className={styles.container}>
+            {props.children}
+        </div >
     );
 };
 
