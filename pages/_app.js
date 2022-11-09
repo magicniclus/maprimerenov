cdac6cffcb81d435d352abe7d9cec0cf96ba0137
Nicolas <casteranicolas.contact@gmail.com>
2022-11-09 18:09:43 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,27 @@ import TagManager from 'react-gtm-module';
 import { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import store from "../redux/store"
+import { authenticateUser } from '../api/Auth';
+import { currentUser, switchConnect } from '../redux/action';
 
 
 function MyApp({ Component, pageProps }) {
-    
+
     useEffect(()=>{
         const tagManagerArgs = {
             gtmId:"GTM-PHVP55Z"
         }
         
         TagManager.initialize(tagManagerArgs)
+    }, [])
+
+    useEffect(()=>{
+        authenticateUser().then(user=>{
+            store.dispatch(switchConnect(true))
+            store.dispatch(currentUser(user))
+        }).catch(err=>{
+            switchConnect(false)
+        })
     }, [])
 
     return (
