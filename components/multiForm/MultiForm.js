87314268eb07b907a339727c98d1a08bda3de053
Nicolas <casteranicolas.contact@gmail.com>
2022-11-10 20:01:13 +0100

    Update MultiForm.js

--- components/multiForm/MultiForm.js
+++ components/multiForm/MultiForm.js
@@ -174,42 +174,50 @@ const MultiForm = () => {
 
     const addPropspectProcess = ()=>{
         setLoader(true)
-            addDoc(databaseRef, {
-                name: propsect.name,
-                phone: propsect.phone,
-                zipCode: propsect.zipCode,
-                email: propsect.email,
-                contract: propsect.contract,
-                type: propsect.type,
-                status: propsect.status,
-                years: propsect.years,
-                size: propsect.size,
-                heater: propsect.heater,
-                isolation: propsect.isolation,
-                fenetre: propsect.fenetre,
-                vmc: propsect.vmc,
-                pompeAChaleurClim: propsect.pompeAChaleurClim,
-                chauffage: propsect.chauffage,
-                solaireChauffeEau: propsect.solaireChauffeEau,
-                date: propsect.date,
-                createAt: propsect.createAt,
-                attribution: propsect.attribution,
-                call: propsect.call,
-                callNumer: propsect.callNumer,
-                click: propsect.click,
-                send: propsect.send,
-                comment: propsect.comment,
-                sign: propsect.sign,
-                signPrestation: propsect.signPrestation
-            })
-            .then(async () => {
-                await Router.push('/merci')
-                await setLoader(false)
-            })
-            .catch((err) => {
-                console.error(err)
-                setLoader(false)
-            })
+        addDocs()
+    }
+
+    const addDocs = ()=>{
+        addDoc(databaseRef, {
+            name: propsect.name,
+            phone: propsect.phone,
+            zipCode: propsect.zipCode,
+            email: propsect.email,
+            contract: propsect.contract,
+            type: propsect.type,
+            status: propsect.status,
+            years: propsect.years,
+            size: propsect.size,
+            heater: propsect.heater,
+            isolation: propsect.isolation,
+            fenetre: propsect.fenetre,
+            vmc: propsect.vmc,
+            pompeAChaleurClim: propsect.pompeAChaleurClim,
+            chauffage: propsect.chauffage,
+            solaireChauffeEau: propsect.solaireChauffeEau,
+            date: propsect.date,
+            createAt: propsect.createAt,
+            attribution: propsect.attribution,
+            call: propsect.call,
+            callNumer: propsect.callNumer,
+            click: propsect.click,
+            send: propsect.send,
+            comment: propsect.comment,
+            sign: propsect.sign,
+            signPrestation: propsect.signPrestation
+        })
+        .then(async () => {
+            await Router.push('/merci')
+            await setLoader(false)
+        })
+        .catch((err) => {
+            console.error(err)
+            setLoader(false)
+        })
+    }
+
+    const creatUser = ()=>{
+
     }
 
     const prevValue = (e) => {
