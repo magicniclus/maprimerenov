708cf85522e54dea1d5c4328f555eee7dda05565
Nicolas <casteranicolas.contact@gmail.com>
2022-11-10 12:24:19 +0100

    Update index.js

--- pages/mon-espace/index.js
+++ pages/mon-espace/index.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react';
 import styles from "./styles/monEspace.module.css"
 import LayoutClassicPage from '../../components/classicPage/LayoutClassicPage';
 import { getUser, updateUser } from '../../api/Auth';
-import { currentUser } from '../../redux/action';
 import { useSelector } from 'react-redux';
 import Router from 'next/router';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const index = () => {
 
@@ -12,38 +12,39 @@ const index = () => {
 
     const state = useSelector(state=>state)
 
+    const [loader, setLoader] = useState(true)
+
     useEffect(()=>{
-        if(!state.areConnect)
-        Router.push("/connection")
-        console.log("test1");
+        if(!state.areConnect){
+            Router.push("/connection")
+            setLoader(true)
+        }else{
+            setLoader(false)
+        }
     }, [state.areConnect])
 
     useEffect(()=>{
         getUser().then(currentUsers=>{
             console.log(currentUsers);
             setUserName(currentUsers.displayName)
-            console.log("test2");
         }).catch(err=>err)
     }, [])
-
-    const handleUser = ()=>{
-        updateUser("Nicolas").then(user=>console.log(user))
-        .catch(err=>{
-            alert(err.message)
-            console.log("test3");
-        })
-    }
 
     return (
         <LayoutClassicPage title="MaPrimeRenov-info | Simulateur MaPrimeRenov" meta="Simulateur gratuit pour connaitre votre éligibilité aux aides de l'état MaPrimeRenov 2022.">
             <main className={styles.main}>
+                {
+                    loader ?
+                    <div className={styles.loader}>
+                        <CircularProgress />
+                    </div> : null
+                }
                 <h1 className={styles.title} style={{color: "black"}}>
                     Hello {
                         userName !== "" ? 
                         userName : null
                     }
                 </h1>
-                {/* <button onClick={handleUser}>Click Here</button> */}
             </main>
         </LayoutClassicPage>
     );
