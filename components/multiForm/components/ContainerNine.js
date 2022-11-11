babd44b634fb61e4434b22a2bf81e750b45331dd
Nicolas <casteranicolas.contact@gmail.com>
2022-11-11 20:04:25 +0100

    Update ContainerNine.js

--- components/multiForm/components/ContainerNine.js
+++ components/multiForm/components/ContainerNine.js
@@ -8,6 +8,7 @@ const ContainerNine = (props) => {
     const valid = props.valid
 
     const [passwordIsTheSame, setPasswordIsSame] = useState(false)
+    const [wordTapping, setWordTapping]=useState(false)
 
     const [passwordOne, setPassWordOne] = useState("")
     const [passwordTwo, setPassWordTwo] = useState("") 
@@ -19,23 +20,25 @@ const ContainerNine = (props) => {
     //     valid(true)
     // }
 
+    useEffect(()=>{
+        if(passwordTwo===passwordOne && passwordTwo !== "" && passwordOne !== "") setPasswordIsSame(true)
+        else setPasswordIsSame(false)
+    }, [passwordTwo, passwordOne])
+
     const handlePasseWordOne = (e)=>{
         setPassWordOne(e.target.value)
     }
 
-    const handlePasseWordTwo = async (e)=>{
-        await setPassWordTwo(e.target.value)
-        if(passwordOne.value === passwordTwo.value )  await setPasswordIsSame(true)
-        if(passwordTwo.value !== passwordOne.value) setPasswordIsSame(false)
+    const handlePasseWordTwo = (e)=>{
+        setPassWordTwo(e.target.value)
     }
-
     return (
         <div className={`${styles.containerThree} ${styles.container}`}>
             <label className={styles.labelCreatAcount}>
                 <h3 className={styles.nameH3}>
                     Bonjour <strong>{name}</strong>,<br/> Creez votre compte
                 </h3>
-                <span className={styles.nameSpan}>Vous y trouverez le resultat de votre simulation</span>
+                <span className={styles.span}>Vous y trouverez le resultat de votre simulation</span>
             </label>
             <div className={`${styles.cardContainerThree} ${styles.cardContainer}`}>
                 <label className={styles.labelAccount}>
@@ -49,9 +52,10 @@ const ContainerNine = (props) => {
                 </label>
                 <label className={styles.labelAccount}>
                     Mot de passe *
-                    <input onChange={handlePasseWordOne} className={styles.input} type="password" required placeholder='Mot de passe' />
-                    <input onChange={handlePasseWordTwo} className={styles.input} type="password" required placeholder='Confirmation du mot de passe' />
+                    <input onChange={e=>handlePasseWordOne(e)} className={styles.input} type="password" style={passwordIsTheSame ? {border: "1px solid green"} : null} required placeholder='Mot de passe' />
+                    <input onChange={e=>handlePasseWordTwo(e)} className={styles.input} type="password" style={passwordIsTheSame ? {border: "1px solid green"} : null} required placeholder='Confirmation du mot de passe' />
                 </label>
+                <span className={styles.lastSpan} style={{color: "#314662"}}>Le resultat de cette simulation n'est pas une proposition commerciale et l'ensemble des resultats obtenus devront être validés par un expert.</span>
             </div>
         </div>
     );
