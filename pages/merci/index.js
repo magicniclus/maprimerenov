6cf43301c562ae5fbf0de80bde1bd8fed69a554e
Nicolas <casteranicolas.contact@gmail.com>
2022-11-12 23:03:22 +0100

    Update index.js

--- pages/merci/index.js
+++ pages/merci/index.js
@@ -7,7 +7,8 @@ const merci = () => {
         <LayoutClassicPage title="Remerciement" meta="Remerciement poour la validation du formulaire de MaPrimeRenov.">
             <main className={styles.main}>
                 <h1 className={styles.title}>Merci</h1>
-                <h2 className={styles.susTitle}>Nous vous contacterons dans les 24/48h</h2>
+                <h2 className={styles.susTitle}>Vous allez recevoir un email de confirmation pour acceder à votre espace.</h2>
+                <p className={styles.p}>Si il n'apparait pas verifiez vos spams</p>
             </main>
         </LayoutClassicPage>
     );
