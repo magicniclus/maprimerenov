f9f79aa4a60558ad099d2ccbf83559f0f90cd06c
Nicolas <casteranicolas.contact@gmail.com>
2022-11-12 23:03:03 +0100

    Update Doc.js

--- api/Doc.js
+++ api/Doc.js
@@ -1,12 +1,21 @@
 import { dataBase } from '../utils/firebase.config';
-import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import { collection, addDoc, doc, getDocFromCache, query, where, getDoc, getDocs } from 'firebase/firestore';
+import { resolve } from 'styled-jsx/css';
 
 const dataBaseMaPrimeRenov = collection(dataBase, 'primeRenovProspect');
 
 export const addDocs = (object)=>{
     return new Promise((resolve, reject)=>{
-        addDoc(dataBaseMaPrimeRenov, object).then(success =>{
+        addDoc(dataBaseMaPrimeRenov, object).then(() =>{
             resolve(true)
         }).catch(err=>reject(err))
     })
 }
+
+// export const showUserInformation =async (uid)=>{
+//     const q = query(collection(dataBase, "primeRenovProspect"));
+
+//     const querySnapshot = await getDocs(q);
+
+//     console.log(querySnapshot);
+// }
