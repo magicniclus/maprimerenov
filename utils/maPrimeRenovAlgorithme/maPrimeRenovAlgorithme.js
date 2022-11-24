f98a633d438a9216b9dfddc2cc851f45f5c34b1c
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-11-24 17:36:35 +0100

    Update maPrimeRenovAlgorithme.js

--- utils/maPrimeRenovAlgorithme/maPrimeRenovAlgorithme.js
+++ utils/maPrimeRenovAlgorithme/maPrimeRenovAlgorithme.js
@@ -1,7 +1,50 @@
-export const maPrimeRenovAlgoritme = (data, lieu, number, numberMin, numberMax)=>{
-    Object.values(data[lieu][number]).forEach(el =>{
-        if(el.max !== undefined ){
-            console.log(el.max);
+export const maPrimeRenovAlgoritme = (data, lieu, number, revenusMin, revenusMax)=>{
+    console.log(revenusMin);
+    Object.values(data[lieu][updateNumber(number)]).forEach((el, idx )=>{
+        if(el.max === undefined && revenusMax === undefined ){
+            console.log("rose")
         }
     })
+}
+
+const updateColor = (idx) =>{
+    switch (idx) {
+        case 0:
+            return "bleu"
+        
+        case 1:
+            return "jaune"
+
+        case 2:
+            return "violet"
+
+        case 3:
+            return "rose"  
+
+        default:
+            return;
+    }
+}
+
+const updateNumber = (nbr)=>{
+    // console.log(nbr);
+    switch (nbr) {
+        case 1:
+            return "one"
+        
+        case 2:
+            return "two"
+
+        case 3:
+            return "three"
+
+        case 4:
+            return "four"
+            
+        case 5: 
+            return "five"    
+
+        default:
+            return "one";
+    }
 }
