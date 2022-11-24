121cb057954e2cf0bc82fee775c6b87ede1e03ae
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-11-24 14:07:05 +0100

    Update ContainerHeight.js

--- components/multiForm/components/ContainerHeight.js
+++ components/multiForm/components/ContainerHeight.js
@@ -7,23 +7,34 @@ const ContainerHeight = (props) => {
     const value = props.value
 
     const [lastClick, setLastClick] = useState("")
+    const [updateValue, setUpdateValue] = useState("")
 
     useEffect(() => {
-        lastClick !== "" ? valid(true) : valid(false)
-        if (lastClick !== "") {
+        updateValue !== "" ? valid(true) : valid(false)
+        if (updateValue !== "") {
             valid(true)
         }
-        value(lastClick)
-    }, [lastClick])
+        value(updateValue)
+    }, [updateValue])
 
-    const handleClick = (name) => {
-        setLastClick(lastClick !== name ? name : "")
+    const handleClick = async (name) => {
+        if(name === "Inférieur à 31 003€") setUpdateValue({min: 0, max: 31003})
+        if(name === "Entre 31 003€ et 37 739€") setUpdateValue({min: 31003, max: 37739})
+        if(name === "Entre 37 739€ et 56 130€") setUpdateValue({min: 37739, max: 56130})
+        if(name === "Supérieur à 56 130€") setUpdateValue({min: 56130})
+
+        if(name === "Inférieur à 31 003€" && updateValue.min == 0)setUpdateValue("")
+        if(name === "Entre 31 003€ et 37 739€" && updateValue.min === 31003)setUpdateValue("")
+        if(name === "Entre 37 739€ et 56 130€" && updateValue.min === 37739)setUpdateValue("")
+        if(name === "Supérieur à 56 130€" && updateValue.min === 56130)setUpdateValue("")
     }
 
     const handleValid = (name) => {
-        if (name === lastClick) return true
-        return false
+        if(updateValue === "") return false
+        if (name.min === updateValue.min) return true
+        else return false
     }
+
     return (
         <div className={`${styles.containerHeight} ${styles.container}`}>
             <label className={styles.label}>
@@ -31,10 +42,10 @@ const ContainerHeight = (props) => {
                 <span className={styles.span}>Cela nous permet d'être plus precis pour le calcul de vos aides</span>
             </label>
             <div className={`${styles.cardContainerSice} ${styles.cardContainer}`}>
-                <SmallCard setLastClick={(e) => handleClick("Inférieur à 31 003€")} valid={handleValid("Inférieur à 31 003€")} title="Inférieur à 31 003€" />
-                <SmallCard setLastClick={(e) => handleClick("Entre 31 003€ et 37 739€")} valid={handleValid("Entre 31 003€ et 37 739€")} title="Entre 31 003€ et 37 739€" />
-                <SmallCard setLastClick={(e) => handleClick("Entre 37 739€ et 56 130€")} valid={handleValid("Entre 37 739€ et 56 130€")} title="Entre 37 739€ et 56 130€" />
-                <SmallCard setLastClick={(e) => handleClick("Supérieur à 56 130€")} valid={handleValid("Supérieur à 56 130€")} title="Supérieur à 56 130€" />
+                <SmallCard setLastClick={(e) => handleClick("Inférieur à 31 003€")} valid={handleValid({min: 0, max: 31003})} title="Inférieur à 31 003€" />
+                <SmallCard setLastClick={(e) => handleClick("Entre 31 003€ et 37 739€")} valid={handleValid({min: 31003, max: 37739})} title="Entre 31 003€ et 37 739€" />
+                <SmallCard setLastClick={(e) => handleClick("Entre 37 739€ et 56 130€")} valid={handleValid({min: 37739, max: 56130})} title="Entre 37 739€ et 56 130€" />
+                <SmallCard setLastClick={(e) => handleClick("Supérieur à 56 130€")} valid={handleValid({min: 56130})} title="Supérieur à 56 130€" />
             </div>
         </div>
     );
