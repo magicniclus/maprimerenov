b5313b0d9f8c00a045e04df8537511ba5e65a19c
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-11-28 17:59:09 +0100

    Update ContainerEleven.js

--- components/multiForm/components/ContainerEleven.js
+++ components/multiForm/components/ContainerEleven.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import { zipCodeRegex } from '../../../utils/regex';
 import styles from "../styles/multiForm.module.css";
 
 const ContainerEleven = (props) => {
@@ -13,8 +14,8 @@ const ContainerEleven = (props) => {
     const handleText = (e)=>{
         e.preventDefault()
         value(e.target.value)
-        if(e.target.value.lenght <= 0) valid(false)
-        valid(true)
+        if(e.target.value.lenght >= 0 ) valid(true)
+        valid(false)
     }
 
     return (
