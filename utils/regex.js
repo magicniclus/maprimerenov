c859d48021fcd309a81ccb12f1ae60ac083a44a6
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-11-28 17:59:17 +0100

    Update regex.js

--- utils/regex.js
+++ utils/regex.js
@@ -1,2 +1,4 @@
 export const emailRegex = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
 export const passwordRegex = new RegExp(/^[A-Za-z]\w{7,14}$/);
+export const zipCodeRegex = new RegExp(/[0-9]{5}/g)
+export const phoneRegex = new RegExp(/(0|\\+33|0033)[1-9][0-9]{8}/)
