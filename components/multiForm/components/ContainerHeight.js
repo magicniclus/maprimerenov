b6ca98abf8b35da11dcbf9190468997812162f14
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-11-29 14:08:52 +0100

    Update ContainerHeight.js

--- components/multiForm/components/ContainerHeight.js
+++ components/multiForm/components/ContainerHeight.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import {maPrimeRenovData} from "../../../utils/maPrimeRenovAlgorithme/maPrimeRenovData"
+import { maPrimeRenovData } from '../../../utils/maPrimeRenovAlgorithme/maPrimeRenovData';
 import styles from "../styles/multiForm.module.css";
 import SmallCard from './SmallCard';
+import { Skeleton } from '@mui/material';
+import { updateNumber } from '../../../utils/maPrimeRenovAlgorithme/maPrimeRenovAlgorithme';
 
 const ContainerHeight = (props) => {
 
@@ -10,6 +12,8 @@ const ContainerHeight = (props) => {
 
     const valid = props.valid
     const value = props.value
+
+    const [vignetteLoader, setVignetteLoader]=useState(true)
 
     const [updateValue, setUpdateValue] = useState("")
 
@@ -48,8 +52,12 @@ const ContainerHeight = (props) => {
     }
 
     useEffect(()=>{
-        console.log(maPrimeRenovData.province["one"]["bleu"]);
-    }, [])
+        if(state.getZipCode !== null && state.numberOfFamily !== null ){
+            setVignetteLoader(false)
+        }else{
+            setVignetteLoader(true)
+        }
+    }, [state])
 
     return (
         <div className={`${styles.containerHeight} ${styles.container}`}>
@@ -58,10 +66,26 @@ const ContainerHeight = (props) => {
                 <span className={styles.span}>Cela nous permet d'être plus precis pour le calcul de vos aides</span>
             </label>
             <div className={`${styles.cardContainerSice} ${styles.cardContainer}`}>
-                <SmallCard setLastClick={(e) => handleClick("blue")} valid={handleValid("blue")} title={"Inférieur à "} />
-                <SmallCard setLastClick={(e) => handleClick("yellow")} valid={handleValid("yellow")} title="Entre 31 003€ et 37 739€" />
-                <SmallCard setLastClick={(e) => handleClick("purple")} valid={handleValid("purple")} title="Entre 37 739€ et 56 130€" />
-                <SmallCard setLastClick={(e) => handleClick("pink")} valid={handleValid("pink")} title="Supérieur à 56 130€" />
+                {
+                    !vignetteLoader ? 
+                    <SmallCard setLastClick={(e) => handleClick("blue")} valid={handleValid("blue")} title={"Inférieur à " + maPrimeRenovData[state.getZipCode][updateNumber(state.numberOfFamily)]["bleu"]["max"]+"€"} />:
+                    <Skeleton variant="rectangular" style={{ width: "132px", minHeight: "113px" }} />
+                }
+                {
+                    !vignetteLoader ? 
+                    <SmallCard setLastClick={(e) => handleClick("yellow")} valid={handleValid("yellow")} title={"Entre "+ maPrimeRenovData[state.getZipCode][updateNumber(state.numberOfFamily)]["jaune"]["min"] +"€ et " + maPrimeRenovData[state.getZipCode][updateNumber(state.numberOfFamily)]["jaune"]["max"] + "€"} />:
+                    <Skeleton variant="rectangular" style={{ width: "132px", minHeight: "113px" }} />
+                }
+                {
+                    !vignetteLoader ? 
+                    <SmallCard setLastClick={(e) => handleClick("purple")} valid={handleValid("purple")} title={"Entre "+ maPrimeRenovData[state.getZipCode][updateNumber(state.numberOfFamily)]["violet"]["min"] +"€ et " + maPrimeRenovData[state.getZipCode][updateNumber(state.numberOfFamily)]["violet"]["max"] + "€"} />:
+                    <Skeleton variant="rectangular" style={{ width: "132px", minHeight: "113px" }} />
+                }
+                {
+                    !vignetteLoader ? 
+                    <SmallCard setLastClick={(e) => handleClick("pink")} valid={handleValid("pink")} title={"Supérieur à " + maPrimeRenovData[state.getZipCode][updateNumber(state.numberOfFamily)]["rose"]["min"]+"€"} />:
+                    <Skeleton variant="rectangular" style={{ width: "132px", minHeight: "113px" }} />
+                }
             </div>
         </div>
     );
