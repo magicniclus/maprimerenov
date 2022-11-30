3d74872e97915eb9c2b572f8e5b845747df3e1cf
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-11-30 15:01:59 +0100

    Update Simulateur.js

--- components/simulateur/Simulateur.js
+++ components/simulateur/Simulateur.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import Cards from './components/Cards';
 import styles from "./styles/simulateur.module.css";
 
 const Simulateur = (props) => {
-    const prestations = props.prestations;
+    const prestations = props.prestations
+    const revenusColor = props.revenusColor
+    const [loader, setLoader]=useState(true)
+    const [awaitMessage, setAwaitMessage]=useState("Veuillez patienter...")
+
+
+    useEffect(()=>{
+        if(prestations !== []) setLoader(false)
+    }, [prestations, revenusColor])
+
     return (
         <div className={styles.simulateur}>
             <h3 className={styles.simulateur_h3}>Calcul de vos aides:</h3>
             <div className={styles.simulateur_cardContainer}>
                 {
-                    prestations === undefined ? 
-                    <h4 className={styles.simulateur_h4}>Oups !!! Vous n'avez aucun projet en cours...</h4>:
-                    null
+                    loader ? 
+                    <h4 className={styles.simulateur_h4}>{awaitMessage}</h4>:
+                    prestations.map(presta=>
+                        <Cards prestation={presta}/>
+                    )
                 }
             </div>
         </div>
