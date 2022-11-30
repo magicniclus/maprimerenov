98f870aa2ddd1f0822a7d77d2824adb0d771afdf
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-11-30 15:02:04 +0100

    Create maPRimeRenovWorksData.js

---
+++ utils/maPrimeRenovAlgorithme/maPRimeRenovWorksData.js
@@ -0,0 +1,222 @@
+export const maPrimeRenovWorksData = {
+    isolation:{
+        blue:{
+            iti: 25,
+            ite: 75,
+            terrasse: 75,
+            rampant: 25,
+            moyenne: 50,
+            max: 75
+        },
+        yellow:{
+            iti: 20,
+            ite: 60,
+            terrasse: 60,
+            rampant: 20,
+            moyenne: 40,
+            max: 60
+        },
+        purple:{
+            iti: 15,
+            ite: 40,
+            terrasse: 40,
+            rampant: 15,
+            moyenne: 27.5,
+            max: 40
+        },
+        pink:{
+            iti: 7,
+            ite: 15,
+            terrasse: 15,
+            rampant: 7,
+            moyenne: 11,
+            max: 15
+        }
+    },
+    fenetre:{
+        blue:{
+            fenetre: 100,
+            moyenne: 100,
+            max: 100
+        },
+        yellow:{
+            fenetre: 80,
+            moyenne: 80,
+            max: 80
+        },
+        purple:{
+            fenetre: 40,
+            moyenne: 40,
+            max: 40
+        },
+        pink:{
+            fenetre: 0,
+            moyenne: 0,
+            max: 0
+        }
+    },
+    vmc:{
+        blue:{
+            vmcDoubleFlux: 4000,
+            moyenne: 4000,
+            max: 4000
+        },
+        yellow:{
+            vmcDoubleFlux: 3000,
+            moyenne: 3000,
+            max: 3000
+        },
+        purple:{
+            vmcDoubleFlux: 2000,
+            moyenne: 2000,
+            max: 2000
+        },
+        pink:{
+            vmcDoubleFlux: 0,
+            moyenne: 0,
+            max: 0
+        }
+    },
+    pompeAChaleurClim:{
+        blue:{
+            geoThermique: 11000,
+            airEau: 5000,
+            moyenne: 8000,
+            max: 11000
+        },
+        yellow:{
+            geoThermique: 9000,
+            airEau: 4000,
+            moyenne: 6500,
+            max: 9000
+        },
+        purple:{
+            geoThermique: 5000,
+            airEau: 3000,
+            moyenne: 4000,
+            max: 5000
+        },
+        pink:{
+            geoThermique: 0,
+            airEau: 0,
+            moyenne: 0,
+            max: 0
+        }
+    },
+    chauffage:{
+        blue:{
+            chaudiereGranules: 11000,
+            chaudiereBuche: 9000,
+            chaudiereGaz: 1200,
+            poeleGranules: 3000,
+            poeleBuche: 2500,
+            insert: 2500,
+            moyenne: 4800,
+            max: 11000
+        },
+        yellow:{
+            chaudiereGranules: 9000,
+            chaudiereBuche: 7500,
+            chaudiereGaz: 800,
+            poeleGranules: 2500,
+            poeleBuche: 2000,
+            insert: 1500,
+            moyenne: 7760,
+            max: 9000
+        },
+        purple:{
+            chaudiereGranules: 4000,
+            chaudiereBuche: 4000,
+            chaudiereGaz: 0,
+            poeleGranules: 1500,
+            poeleBuche: 1000,
+            insert: 800,
+            moyenne: 2880,
+            max: 4000
+        },
+        pink:{
+            chaudiereGranules: 0,
+            chaudiereBuche: 0,
+            chaudiereGaz: 0,
+            poeleGranules: 0,
+            poeleBuche: 0,
+            insert: 0,
+            moyenne: 0,
+            max: 0
+        }
+    },
+    solaireChauffeEau:{
+        blue:{
+            chauffageSolaire: 11000,
+            chauffeEauSolaire: 4000,
+            equipementSolaireHybride: 2500,
+            moyenne: 5830,
+            max: 11000
+        },
+        yellow:{
+            chauffageSolaire: 9000,
+            chauffeEauSolaire: 3000,
+            equipementSolaireHybride: 2000,
+            moyenne: 4660,
+            max: 11000           
+        },
+        purple:{
+            chauffageSolaire: 5000,
+            chauffeEauSolaire: 2000,
+            equipementSolaireHybride: 1000,
+            moyenne: 2660,
+            max: 11000
+        },
+        pink:{
+            chauffageSolaire: 0,
+            chauffeEauSolaire: 0,
+            equipementSolaireHybride: 0,
+            moyenne: 0,
+            max: 0
+        }
+    },
+    renovationGlobal:{
+        blue:{
+            renovationGlobal: 15000,
+            moyenne: 15000,
+            max: 15000
+        },
+        yellow:{
+            renovationGlobal: 10500,
+            moyenne: 10500,
+            max: 10500
+        },
+        purple:{
+            renovationGlobal: 7000,
+            moyenne: 7000,
+            max: 7000
+        },
+        pink:{
+            renovationGlobal: 3500,
+            moyenne: 3500,
+            max: 3500
+        }
+    },
+    audit:{
+        blue:{
+            audit: 500,
+            moyenne: 500,
+            max: 500
+        },
+        yellow:{
+            audit: 400,
+            moyenne: 400,
+            max: 400
+        },
+        purple:{
+            audit: 300,
+            moyenne: 300,
+            max: 300
+        },
+        pink:{
+            audit: 0,
+            moyenne: 0,
+            max: 0
+        } 
+    }
+}
