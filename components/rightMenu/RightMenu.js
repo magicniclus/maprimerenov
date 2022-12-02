9efe8223ff6b3ddbace15f7f44660d9ee0eb0bf1
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-02 14:56:56 +0100

    Create RightMenu.js

---
+++ components/rightMenu/RightMenu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from "./styles/rightMenu.module.css"
+import Link from 'next/link';
+import Image from 'next/image';
+import { Button } from '@mui/material';
+
+const RightMenu = () => {
+    return (
+        <div className={styles.rightMenu}>
+            
+        </div>
+    );
+};
+
+export default RightMenu;
