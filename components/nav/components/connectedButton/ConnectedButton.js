febe27b420672b6f163ff2db215da3a02a704ca6
Nicolas <72094122+magicniclus@users.noreply.github.com>
2022-12-02 14:56:50 +0100

    Create ConnectedButton.js

---
+++ components/nav/components/connectedButton/ConnectedButton.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import styles from "./styles/connectedButton.module.css"
+import Image from 'next/image';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleMenu } from '../../../../redux/action';
+
+const ConnectedButton = (props) => {
+
+    const dispatch = useDispatch()
+
+    const state = useSelector(state=>state)
+    const [userName, setUserName] = useState(null)
+    const click = props.click
+
+    useEffect(()=>{
+        setUserName(state.userProjectInformation.name)
+    }, [])
+
+    const openToggleMenu = (e)=>{
+        e.preventDefault()
+        click(true) 
+        dispatch(toggleMenu(true))
+    }
+
+    return (
+        <div className={styles.connectedButton} onClick={openToggleMenu}>
+                <p className={styles.p}>Menu</p>
+                <div className={styles.imageContainer}>
+                    <Image priority src="/arrowarrow.png" alt="arrow" width="55px" height="16px" objectFit="contain" />
+                </div>
+        </div>
+    );
+};
+
+export default ConnectedButton;
