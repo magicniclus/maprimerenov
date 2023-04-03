2612759635b21a07160d52159861057da142cf93
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-04-03 09:25:33 +0200

    Update VideoContainer.js

--- components/videoContainer/VideoContainer.js
+++ components/videoContainer/VideoContainer.js
@@ -1,46 +1,73 @@
-import React from 'react';
+import React from "react";
 import styles from "./styles/videoContainer.module.css";
-import { Button} from '@mui/material';
+import { useRouter } from "next/router";
 
 const VideoContainer = () => {
-
-    return (
-        <div className={styles.container}>
-            <div className={styles.content}>
-                <div className={styles.leftContainer}>
-                    <h2 className={styles.title}>
-                    Nous vous accompagnons dans chacune des étapes de votre projet
-                    </h2>
-                    <ul className={styles.list}>
-                        <li className={styles.listElement}>Estimation</li>
-                        <li className={styles.listElement}>Validation du projet</li>
-                        <li className={styles.listElement}>Elaboration et dépot du projet </li>
-                        <li className={styles.listElement}>Suivi de la bonne réalisation des travaux </li>
-                        <li className={styles.listElement}>Livraison</li>
-                        <li className={styles.listElement}>Encaissement de la prime par l’état</li>
-                    </ul>
-                    <Button 
-                        variant="contained" 
-                        style={{backgroundColor: "#74c011"}} 
-                        onClick={e =>{
-                        let form = document.getElementById('form')
-                        e.preventDefault()
-                        form && form.scrollIntoView({ behavior: "smooth", block: "start"})
-                    }
-                    } className={styles.button}
-                    >
-                        Tester mon éligibilité
-                    </Button>
-
-                </div>
-                <div className={styles.rightContainer}>
-                    <div className={styles.videoContainer}>
-                        <iframe className={styles.videoYoutube} width="560" height="315" src="https://www.youtube.com/embed/O4HxHLQ3k9Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>
-                </div>
-            </div>
+  const router = useRouter();
+  const locationUrl = router.pathname;
+  const text = {
+    init: {
+      h2: "Nous vous accompagnons dans chacune des étapes de votre projet",
+      li: [
+        "-Estimation",
+        "-Validation du projet",
+        "-Elaboration et dépot du projet",
+        "-Suivi de la bonne réalisation des travaux",
+        "-Livraison",
+        "-Encaissement de la prime par l’état",
+      ],
+    },
+    CAH: {
+      h2: "Nous vous guidons à travers chaque étape de votre projet",
+      li: [
+        "-Évaluation",
+        "-Confirmation du projet",
+        "-Préparation et soumission du projet",
+        "-Contrôle du bon déroulement des travaux",
+        "-Réception",
+        "-Paiement de la prime par l'État",
+      ],
+    },
+  };
+  const headerText = locationUrl === "/simulateur" ? text.init : text.CAH;
+  return (
+    <div className="bg-gray-200 w-full py-24 sm:py-32 flex flex-wrap justify-center items-center ">
+      <div className=" max-w-7xl px-6 lg:px-8 flex flex-col lg:flex-row justify-between">
+        <div className="lg:w-2/6 w-full flex flex-col justify-around">
+          <h2 className="text-xl font-bold tracking-tight text-gray-600 sm:text-2xl">
+            {headerText.h2}
+          </h2>
+          <ul className={styles.list}>
+            {headerText.li.map((el, idx) => (
+              <li className="text-normal font-normal leading-6 text-gray-600">
+                {el}
+              </li>
+            ))}
+          </ul>
+          <a
+            href="#form"
+            className="max-w-[180px] rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 "
+          >
+            Tester mon éligibilité
+          </a>
         </div>
-    );
+        <div className={styles.rightContainer}>
+          <div className={styles.videoContainer}>
+            <iframe
+              className={styles.videoYoutube}
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/O4HxHLQ3k9Y"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoContainer;
