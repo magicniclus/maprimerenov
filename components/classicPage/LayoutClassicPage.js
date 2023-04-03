a45947b1f7a5182b08e18bd6828305f8418ea854
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-04-03 13:48:54 +0200

    Update LayoutClassicPage.js

--- components/classicPage/LayoutClassicPage.js
+++ components/classicPage/LayoutClassicPage.js
@@ -1,63 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import Nav from "../nav/Nav"
-import Head from 'next/head';
+import React, { useEffect, useState } from "react";
+import Nav from "../nav/Nav";
+import Head from "next/head";
 import Footer from "../footer/Footer";
 import { useDispatch, useSelector } from "react-redux";
 import { showPage } from "../../redux/action";
 import RightMenu from "../rightMenu/RightMenu";
-import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
 
 const LayoutClassicPage = (props) => {
-    const dispatch = useDispatch()
-    const state = useSelector(state=>state)
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
 
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-    
-    
-    useEffect(()=>{
-        if(props.monEspace !== "unedifined"){
-            dispatch(showPage(props.monEspace))
-        }else{
-            dispatch(showPage(false))
-        }
-    }, [])
-    
-    useEffect(()=>{
-        if(state.menuIsOpen && document !== undefined){
-            // const body = document.querySelector("body")
-            // body.style.overflowY = "hidden"
-            disableBodyScroll(document)
-            setIsOpen(true)
-        }else {
-            // const body = document.querySelector("body")
-            // body.style.overflowY = "scroll"
-            enableBodyScroll(document)
-            setIsOpen(false)
-        }
-    }, [state.menuIsOpen])
+  useEffect(() => {
+    if (props.monEspace !== "unedifined") {
+      dispatch(showPage(props.monEspace));
+    } else {
+      dispatch(showPage(false));
+    }
+  }, []);
 
-    return (
-        <div>
-            <Head>
-                <title>{props.title}</title>
-                <meta name="description" content={props.meta} />
-                <link rel="shortcut icon" href="imageHeader.png" type="image/x-icon" />
-            </Head>
-                <header>
-                    <Nav />
-                </header>
-                {
-                    isOpen?
-                    <RightMenu />:
-                    null
-                }
-                {props.children}
-                <footer>
-                    <Footer />
-                </footer>
-        </div>
-    );
+  useEffect(() => {
+    if (state.menuIsOpen && document !== undefined) {
+      // const body = document.querySelector("body")
+      // body.style.overflowY = "hidden"
+      disableBodyScroll(document);
+      setIsOpen(true);
+    } else {
+      // const body = document.querySelector("body")
+      // body.style.overflowY = "scroll"
+      enableBodyScroll(document);
+      setIsOpen(false);
+    }
+  }, [state.menuIsOpen]);
+
+  return (
+    <div>
+      <Head>
+        <title>{props.title}</title>
+        <meta charset="UTF-8" />
+        <meta name="description" content={props.meta} />
+        <link rel="shortcut icon" href="imageHeader.png" type="image/x-icon" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="language" content="fr" />
+      </Head>
+      <header>
+        <Nav />
+      </header>
+      {isOpen ? <RightMenu /> : null}
+      {props.children}
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default LayoutClassicPage;
