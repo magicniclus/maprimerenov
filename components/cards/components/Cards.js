c91ced88269f9388449dfe42c829b14d5fad62db
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-04-03 09:25:05 +0200

    Update Cards.js

--- components/cards/components/Cards.js
+++ components/cards/components/Cards.js
@@ -1,30 +1,48 @@
-import React from 'react';
-import styles from '../styles/cards.module.css';
+import React from "react";
+import styles from "../styles/cards.module.css";
 
 const Cards = (props) => {
-    const title = props.title;
-    const text = props.text;
-    const accroche = props.accroche;
+  const title = props.title;
+  const text = props.text;
+  const accroche = props.accroche;
 
-    return (
-        <div className={styles.cards}>
-            <h3 className={styles.title}>
-                {
-                    title !== undefined ? title : null
-                }
-            </h3>
-            <h4 className={styles.text}>
-                {
-                    text !== undefined ? text : null
-                }
-            </h4>
-            <p className={styles.accrorche}>
-                {
-                    accroche !== undefined ? accroche : null
-                }
-            </p>
+  return (
+    <div className="h-80 relative w-80 lg:max-w-[300px] rounded-xl bg-indigo-600 z-10 flex flex-col justify-around p-4 mb-10 lg:mb-0">
+      <div
+        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#818cf8] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
+        <div
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#818cf8] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
         </div>
-    );
+      </div>
+      <h3 className="w-full mx-auto md:text-6xl text-4xl text-gray-100">
+        {title !== undefined ? title : null}
+      </h3>
+      <h4 className={styles.text}>{text !== undefined ? text : null}</h4>
+      <div className="w-full h-16 rounded-md flex justify-center items-center px-3">
+        <p className="text-lg text-white text-center">
+          {accroche !== undefined ? accroche : null}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
