c78c915b96c19509a4eff367a4a84cc9c01f5785
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-04-03 09:25:12 +0200

    Update ContainerTen.js

--- components/multiForm/components/ContainerTen.js
+++ components/multiForm/components/ContainerTen.js
@@ -33,31 +33,31 @@ const ContainerTen = (props) => {
             <label className={styles.label}>
             Combien de personnes composent votre foyer, vous compris ? :*
                 <span className={styles.span}>Le montant des aides varie en fonction de la taille de votre ménage.</span>
-                <select
-                    style={{maxWidth: "64%"}}
-                    onChange={e => {
-                        handleText(e)
+                    <select
+                        style={{maxWidth: "64%"}}
+                        onChange={e => {
+                            handleText(e)
+                        }
                     }
-                }
-                        className={styles.select} name="type" id="type">
-                        <option disabled selected>Selectionnez une option</option>
-                        <option value={1}>1</option>
-                        <option value={2}>2</option>
-                        <option value={3}>3</option>
-                        <option value={4}>4</option>
-                        <option value={5}>5</option>
-                        <option value={6}>6</option>
-                        <option value={7}>7</option>
-                        <option value={8}>8</option>
-                        <option value={9}>9</option>
-                        <option value={10}>10</option>
-                        <option value={11}>11</option>
-                        <option value={12}>12</option>
-                        <option value={13}>13</option>
-                        <option value={13}>13</option>
-                        <option value={14}>14</option>
-                        <option value={15}>15</option>
-                    </select>
+                            className={styles.select} name="type" id="type">
+                            <option disabled selected>Selectionnez une option</option>
+                            <option value={1}>1</option>
+                            <option value={2}>2</option>
+                            <option value={3}>3</option>
+                            <option value={4}>4</option>
+                            <option value={5}>5</option>
+                            <option value={6}>6</option>
+                            <option value={7}>7</option>
+                            <option value={8}>8</option>
+                            <option value={9}>9</option>
+                            <option value={10}>10</option>
+                            <option value={11}>11</option>
+                            <option value={12}>12</option>
+                            <option value={13}>13</option>
+                            <option value={13}>13</option>
+                            <option value={14}>14</option>
+                            <option value={15}>15</option>
+                        </select>
             </label>
             {/* <div className={`${styles.cardContainerTen} ${styles.cardContainer}`}>
                 <input className={`${styles.input} ${styles.inputNumber}`} onChange={e=>handleText(e)} type="number" required placeholder="100" maxLength={15} minLength={15} />
