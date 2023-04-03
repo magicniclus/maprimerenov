4502ae6d43fe2520f0b3b5196050c3422f197d3b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-04-03 09:24:56 +0200

    Update Doc.js

--- api/Doc.js
+++ api/Doc.js
@@ -1,27 +1,48 @@
-import { dataBase } from '../utils/firebase.config';
-import { collection, addDoc, doc, getDocFromCache, query, where, getDoc, getDocs, setDoc } from 'firebase/firestore';
+import { dataBase } from "../utils/firebase.config";
+import {
+  collection,
+  addDoc,
+  doc,
+  getDocFromCache,
+  query,
+  where,
+  getDoc,
+  getDocs,
+  setDoc,
+} from "firebase/firestore";
 
-const dataBaseMaPrimeRenov = collection(dataBase, 'primeRenovProspect');
+const dataBaseMaPrimeRenov = collection(dataBase, "primeRenovProspect");
 
-export const setUserDoc = async (object)=>{
-    return new Promise((resolve, reject)=>{
-        setDoc(doc(dataBase, "primeRenovProspect", object.name), object).then(()=>{
-            resolve(true)
-        }).catch(err=>reject(err))
-    })
-}
+export const setUserDoc = async (object) => {
+  return new Promise((resolve, reject) => {
+    setDoc(doc(dataBase, "primeRenovProspect", object.name), object)
+      .then(() => {
+        resolve(true);
+      })
+      .catch((err) => reject(err));
+  });
+};
 
-export const showUserInformation =async (uid)=>{
-    const q = query(collection(dataBase, "users"), where("uid", "==", uid));
-    const querySnapshot = await getDocs(q);
-    let user;
-    return new Promise(async (resolve)=>{
-        querySnapshot.forEach( (doc) => {
-            console.log(doc.data());
-            user = doc.data()
-        })
-        if(user === undefined) await user
-        else resolve(user)
-    })
-}
+export const setUserDocCAH = async (object) => {
+  return new Promise((resolve, reject) => {
+    setDoc(doc(dataBase, "CAH", object.name), object)
+      .then(() => {
+        resolve(true);
+      })
+      .catch((err) => reject(err));
+  });
+};
 
+export const showUserInformation = async (uid) => {
+  const q = query(collection(dataBase, "users"), where("uid", "==", uid));
+  const querySnapshot = await getDocs(q);
+  let user;
+  return new Promise(async (resolve) => {
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data());
+      user = doc.data();
+    });
+    if (user === undefined) await user;
+    else resolve(user);
+  });
+};
