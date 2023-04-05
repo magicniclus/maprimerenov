0dfa4a6c5372045600e2e8fda4dd54dbed6e7a25
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-04-05 09:31:24 +0200

    Update HeaderForm.js

--- components/headerForm/HeaderForm.js
+++ components/headerForm/HeaderForm.js
@@ -7,11 +7,11 @@ const HeaderForm = () => {
   const locationUrl = router.pathname;
   const text = {
     init: {
-      h1: "Testez votre éligibilité aux aides 2022/2023 encadré par l’état: MaPrimeRenov’!",
+      h1: "Testez votre éligibilité aux aides 2023 encadré par l’état: MaPrimeRenov’!",
       h3: "80% de vos travaux d’économie d’énergie pris en charge.",
     },
     CAH: {
-      h1: "Validez votre éligibilité aux subventions 2022/2023 soutenues par l'État : MaPrimeRenov' !",
+      h1: "Validez votre éligibilité aux subventions 2023 soutenues par l'État : MaPrimeRenov' !",
       h3: "Bénéficiez d'une prise en charge de 80% pour vos projets d'économies d'énergie.",
     },
   };
