3f5281b0985a52775c10a82103e5fe11606558a1
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-04-05 09:31:28 +0200

    Update firebase.config.js

--- utils/firebase.config.js
+++ utils/firebase.config.js
@@ -9,9 +9,25 @@ export const firebaseConfig = {
   projectId: "maprimerenov-58fdf",
   storageBucket: "maprimerenov-58fdf.appspot.com",
   messagingSenderId: "855231284792",
-  appId: "1:855231284792:web:e491151c0617175c01c8cd"
+  appId: "1:855231284792:web:e491151c0617175c01c8cd",
 };
+
+// // Your web app's Firebase configuration CAH
+const firebaseConfig2 = {
+  apiKey: "AIzaSyBWI7ZbXjSp96rI3iLdQtfDhRXezcVwaxk",
+  authDomain: "leadmanager-bb42b.firebaseapp.com",
+  projectId: "leadmanager-bb42b",
+  storageBucket: "leadmanager-bb42b.appspot.com",
+  messagingSenderId: "773911657744",
+  appId: "1:773911657744:web:91654836d922f951eab995",
+};
+
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const dataBase = getFirestore(app);
-export const auth = getAuth(app)
+export const auth = getAuth(app);
+
+// Initialize CAH Firebase
+export const app2 = initializeApp(firebaseConfig2, "app2");
+export const dataBase2 = getFirestore(app2);
+export const auth2 = getAuth(app2);
