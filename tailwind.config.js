f151af818d6f666610bccc7aad269f5aadcfd8a6
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-04-05 15:55:02 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      clearBlue: "#056CF2",
+      darkBlue: "#0A2540",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
