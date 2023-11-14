e27744d3a029fdb743b386613d552365f83cade1
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-11-14 19:53:23 +0100

    Update ContainerThree.js

--- components/multiForm/components/ContainerThree.js
+++ components/multiForm/components/ContainerThree.js
@@ -1,33 +1,39 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import styles from "../styles/multiForm.module.css";
 
 const ContainerThree = (props) => {
+  const valid = props.valid;
+  const value = props.value;
 
-    const valid = props.valid
-    const value = props.value
+  useEffect(() => {
+    value("");
+  }, []);
 
-    useEffect(()=>{
-        value("")
-    }, [])
+  const handleText = (e) => {
+    e.preventDefault();
+    value(e.target.value);
+    if (e.target.value.lenght <= 0) valid(false);
+    valid(true);
+  };
 
-    const handleText = (e)=>{
-        e.preventDefault()
-        value(e.target.value)
-        if(e.target.value.lenght <= 0) valid(false)
-        valid(true)
-    }
-
-    return (
-        <div className={`${styles.containerThree} ${styles.container}`}>
-            <label className={styles.label}>
-                Surface habitable de votre logement :*
-                <span className={styles.span}>En m2</span>
-            </label>
-            <div className={`${styles.cardContainerThree} ${styles.cardContainer}`}>
-                <input className={`${styles.input} ${styles.inputNumber}`} onChange={e=>handleText(e)} type="number" required placeholder="100" minLength={0} />
-            </div>
-        </div>
-    );
+  return (
+    <div className={`${styles.containerThree} ${styles.container}`}>
+      <label className={styles.label}>
+        Surface habitable de votre logement :*
+        <span className={styles.span}>En m2</span>
+      </label>
+      <div className={`${styles.cardContainerThree} ${styles.cardContainer}`}>
+        <input
+          className={`${styles.input} ${styles.inputNumber}`}
+          onChange={(e) => handleText(e)}
+          type="number"
+          required
+          placeholder="100"
+          minLength={0}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ContainerThree;
